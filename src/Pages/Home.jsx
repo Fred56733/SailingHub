f85f189aff4ce913738642fe10c7d0568d04f09b
Fred56733 <fred.debiase55@gmail.com>
2025-04-21 16:07:02 -0400

    added posts to display in the home screen

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../client";
+import './Home.css';
 
 const Home = () => {
+    const [posts, setPosts] = useState([]);
+
+    // Fetch data from Supabase
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data, error } = await supabase
+                .from('sailing posts')
+                .select('*');
+            if (error) console.error("Error fetching data:", error);
+            else {
+               console.log("Fetched data:", data); 
+                setPosts(data); // Set the fetched data to state
+            } 
+        };
+        fetchData();
+    }, []);
+        
+
     return (
         <div className="home">
         <h1>Welcome to Sailing Hub</h1>
         <p>Explore and create your own unique posts to others in the sailing community!</p>
         <p>Use the navigation bar at the top of the page to get started.</p>
+        <div className="posts-grid">
+            {posts.map((post) => (
+                <div key={post.id} className="post-card">
+                    {post.image && <img src={post.image} alt={""} className="posts-card-img" />}
+                    <h2>{post.title}</h2>
+                    <p>{post.content}</p>
+                </div>
+            ))}
+        </div>
         </div>
     );
 }
