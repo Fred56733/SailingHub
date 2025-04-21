a0844d646f5e86a6db58268a097be1b91e1c439b
Fred56733 <fred.debiase55@gmail.com>
2025-04-21 14:34:38 -0400

    allows users to submit posts

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './Create.css'; 
+import { supabase } from "../client";
 
 const Create = () => {
+    
+    const handleSubmit = async (event) => {
+        event.preventDefault(); 
+
+        const {error} = await supabase
+            .from('sailing posts')
+            .insert({
+                title: event.target.title.value, 
+                content: event.target.content.value, 
+                image: event.target.image.value 
+            });
+
+        if (error) {
+            console.error("Error inserting data:", error);
+        } else {
+            console.log("Post created successfully!");
+        }
+    }
+
     return (
         <div className="create">
             <h1>Create a New Post</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="title">Title:</label>
                 <input type="text" id="title" name="title" required />
 
