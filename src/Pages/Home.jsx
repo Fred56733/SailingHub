68d3a15b39b3a3d5263dc25b97169de041015afe
Fred56733 <fred.debiase55@gmail.com>
2025-04-21 12:47:26 -0400

    added pages for navigation

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="home">
+        <h1>Welcome to the Crewmate Gallery</h1>
+        <p>Explore and create your own unique crewmates!</p>
+        <p>Use the navigation bar to get started.</p>
+        </div>
+    );
+}
+
+export default Home;
