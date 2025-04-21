69906667d7b82a88c10df9ddcd037a817611e43c
Fred56733 <fred.debiase55@gmail.com>
2025-04-21 13:36:01 -0400

    added a .env for api keys

--- src/client.js
+++ src/client.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const URL = '';
-const API_KEY = '';
+const URL = process.env.REACT_APP_SUPABASE_URL;
+const API_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+console.log("Supabase URL:", URL);
+console.log("Supabase API Key:", API_KEY);
 
 export const supabase = createClient(URL, API_KEY);
