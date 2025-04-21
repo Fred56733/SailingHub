8aca2b00e0ae065eb7104332680fbe5fa4db7a9e
Fred56733 <fred.debiase55@gmail.com>
2025-04-21 12:47:05 -0400

    baisic react project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Create from './Pages/Create';
+import Gallery from './Pages/Gallery';
+import './App.css';
+
+function App() {
+  return (
+      <Router>
+          <Navbar />
+          <Routes>
+              <Route path="/" element={<Home />} /> {/* Home page */}
+              <Route path="/create" element={<Create />} /> { /* For creating new crewmates */}
+              <Route path="/create/:id" element={<Create />} /> {/* For editing */}
+              <Route path="/gallery" element={<Gallery />} /> {/* Crewmate display page */}
+          </Routes>
+      </Router>
+  );
+}
+
+
+export default App
